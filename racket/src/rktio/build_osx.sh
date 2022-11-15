679bcd24c5e4c55558d455cc8ef9b93a7504ca8d
Shawn Presser <shawnpresser@gmail.com>
2022-11-15 16:15:18 -0600

    Update build scripts

--- racket/src/rktio/build_osx.sh
+++ racket/src/rktio/build_osx.sh
@@ -1,9 +1,14 @@
 set -ex
 if [ ! -f Makefile ]
 then
-  ./configure
+  ./configure --enable-standalone
 fi
-make -j7
-make demo.o
-gcc demo.o librktio.a -framework Foundation -liconv -o rktio_demo
-clang -fpic -shared -Wl,-all_load librktio.a -framework Foundation -liconv -o librktio.dylib
+#make -B -j7
+#make -B demo.o
+#make librktio.a
+#gcc demo.o librktio.a -framework Foundation -liconv -o rktio_demo
+#clang -fpic -shared -Wl,-all_load librktio.a -framework Foundation -liconv -o librktio.dylib
+#gcc demo.o librktio.a -framework Foundation -liconv -o rktio_demo
+#make -B librktio.dylib librktio.a rktio_demo
+make -B
+echo "Try running ./rktio_demo -v --stress"
